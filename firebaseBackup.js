2499573183f74013ab0f8cef76ee42fd1ce0bbfc
Abhisek Dubey <116142358+abhiseknevrio@users.noreply.github.com>
2024-01-12 15:25:30 +0530

    Update firebaseBackup.js

--- firebaseBackup.js
+++ firebaseBackup.js
@@ -2,12 +2,11 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: "fir-poc-with-react.firebaseapp.com",
-    projectId: "fir-poc-with-react",
-    storageBucket: "fir-poc-with-react.appspot.com",
-    messagingSenderId: "112042490837",
-    appId: "1:112042490837:web:5616d11898e90b1d8b1b5c",
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
+    authDomain: "******",
+    projectId: "********",
+    storageBucket: "*******com",
+    messagingSenderId: "*******",
+    appId: "******
 };
 
 // Initialize Firebase
